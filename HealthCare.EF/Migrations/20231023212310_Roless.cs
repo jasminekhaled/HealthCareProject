d8a930631c018a8c670da16e428f7f298a705b53
jasminekhaled <jasminekhaled0060@gmail.com>
2024-03-02 11:58:01 +0200

    commit

--- HealthCare.EF/Migrations/20231023212310_Roless.cs
+++ HealthCare.EF/Migrations/20231023212310_Roless.cs
@@ -8,27 +8,7 @@ namespace HealthCare.EF.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.InsertData(
-                table: "Roles",
-                columns: new[] { "Id", "Name" },
-                values: new object[] { 1, "SuperAdmin" }
-            );
-            migrationBuilder.InsertData(
-                table: "Roles",
-                columns: new[] { "Id", "Name" },
-                values: new object[] { 2, "HospitalAdmin" }
-            );
-            migrationBuilder.InsertData(
-                table: "Roles",
-                columns: new[] { "Id", "Name" },
-                values: new object[] { 3, "Patient" }
-            );
-            migrationBuilder.InsertData(
-                table: "Roles",
-                columns: new[] { "Id", "Name" },
-                values: new object[] { 4, "Doctor" }
-                );
-
+            
 
         }
 
