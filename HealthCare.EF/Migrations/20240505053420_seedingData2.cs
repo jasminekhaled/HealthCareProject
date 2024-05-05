9a56a621bcc6cf8125243d8156f6a30a5b5507b5
jasminekhaled <jasminekhaled0060@gmail.com>
2024-05-05 08:46:16 +0300

    commit

---
+++ HealthCare.EF/Migrations/20240505053420_seedingData2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.EF.Migrations
+{
+    public partial class seedingData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: "UploadedFiles",
+               columns: new[] { "Id", "FileName", "StoredFileName", "ContentType", "FilePath" },
+               values: new object[] { 300, "newborn.png", "vlrexppj.xkm", "image/png", "G:\\WEB DEVELOPMENT\\HealthCareProject\\HealthCareAPIs\\HealthCare\\Uploads\\SpecializationImages\\vlrexppj.xkm" }
+           );
+
+            migrationBuilder.InsertData(
+               table: "Specializations",
+               columns: new[] { "Id", "Name", "UploadedFileId" },
+               values: new object[] { 80, "Pediatric surgery", 300 }
+           );
+
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [Specializations]");
+            migrationBuilder.Sql("DELETE FROM [UploadedFiles]");
+        }
+    }
+}
